07e51ccef8246f445ecb7011eb8e4f1e2d2e9ece
usuario <user@user.com>
2018-08-26 19:35:46 -0500

    cambio nombres de carpetas proyecto

--- reciclahorro-rails/app/models/vecino.rb
+++
@@ -1,2 +0,0 @@
-class Vecino < ApplicationRecord
-end
--- reciclahorro-rails/test/controllers/vecinos_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class VecinosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
