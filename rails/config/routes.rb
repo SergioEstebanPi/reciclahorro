d45b925185409e9a979f8b7d13b0b2114fced967
usuario <user@user.com>
2018-08-26 22:00:42 -0500

    Agrego gema devise para manejo y registro de sesiones y usuarios

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
   get 'vecinos' => 'vecinos#index'
   resource :vecinos
 end
