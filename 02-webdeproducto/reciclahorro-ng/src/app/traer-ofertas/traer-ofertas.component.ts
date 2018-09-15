4cc32abed1dc677cd325d7cc3f4f7877e0a51704
usuario <user@user.com>
2018-09-15 07:00:07 -0500

    Agrego servicios de usuarios y ofertas configuro estructura de las vistas

--- 02-webdeproducto/reciclahorro-ng/src/app/traer-ofertas/traer-ofertas.component.ts
+++ 02-webdeproducto/reciclahorro-ng/src/app/traer-ofertas/traer-ofertas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OfertasService } from './../services/ofertas.service';
+
 @Component({
   selector: 'app-traer-ofertas',
   templateUrl: './traer-ofertas.component.html',
@@ -7,9 +9,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TraerOfertasComponent implements OnInit {
 
-  constructor() { }
+  ofertas:Array<any>;
+
+  constructor(private _ofertas:OfertasService) {
+    this.ofertas = [{
+  		//id: "",
+  		titulo: "",
+  		descripcion: ""
+  	}];
+  }
 
   ngOnInit() {
+    this.traerOfertas();
+  }
+
+  traerOfertas(){
+  	this._ofertas.traerOfertas()
+  		.subscribe(
+  			respuesta => {
+  				//console.log(respuesta);
+  				this.ofertas = respuesta;
+  			},
+  			error => {
+  				console.log(error);
+  			}
+  		);
+  }
+
+  mostrarOferta(id){
+  	this._ofertas.mostrarOferta(id)
+  		.subscribe(
+  			respuesta => {
+  				console.log(respuesta);
+  				//this.traerOfertas();
+  			},
+  			error => {
+  				console.log();
+  			}
+  		);
+  }
+
+  crearOferta(oferta){
+  	this._ofertas.crearOferta(oferta)
+  		.subscribe(
+  			respuesta => {
+  				console.log(respuesta);
+  				//this.traerofertas();
+  			},
+  			error => {
+  				console.log();
+  			}
+  		);
+  }
+
+  eliminaOferta(id){
+  	let confirmacion = confirm("Estas seguro?");
+  	if(confirmacion){
+	  	this._ofertas.eliminarOferta(id)
+	  		.subscribe(
+	  			respuesta => {
+	  				//console.log(respuesta);
+	  				this.traerOfertas();
+	  			},
+	  			error => {
+	  				console.log();
+	  			}
+	  		);
+  	}
   }
 
 }
